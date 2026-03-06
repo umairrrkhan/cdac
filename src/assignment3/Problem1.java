ebba4b1baf691fc4ebfa2dac416d1c0dd48aee85
umairrrkhan <umair64066@gmail.com>
2026-03-06 16:40:30 +0530

    "problem 1done easy one "

---
+++ src/assignment3/Problem1.java
@@ -0,0 +1,21 @@
+package assignment3;
+
+public class Problem1 {
+	
+	public static void main(String [] args) {
+		int a = 16;
+		
+		if(a<=18) {
+			System.out.println("eligible");
+		}
+		
+		else if(a>18) {
+			System.out.println("not eligible");
+		}
+		
+		else {
+			System.out.println("what u doing put ur age");
+		}
+	}
+
+}
