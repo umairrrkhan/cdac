109ccd5066806f3ca060ceccac47d378900166f7
umairrrkhan <umair64066@gmail.com>
2026-03-06 17:28:51 +0530

    kinda hard but good problem in if else

---
+++ src/assignment3/Problem6.java
@@ -0,0 +1,39 @@
+package assignment3;
+
+import java.util.Scanner;
+
+public class Problem6 {
+	
+	public static void main(String[] args) {
+		Scanner io = new Scanner(System.in	);
+		
+		System.out.print("eneter ur salary = ");
+		int salary = io.nextInt();
+		
+		System.out.println("enter the credit score");
+		int credit_score  =io.nextInt();
+		
+		if(salary >=30000	) {
+			System.out.println("eligible");
+			
+			 if (salary >= 50000 && credit_score >= 750) {
+				System.out.println("Premium Eligible");
+				
+			}
+		}
+		
+		else if (credit_score>=700	) {
+			System.out.println("eligible");
+			 if (salary >= 50000 && credit_score >= 750) {
+				System.out.println("Premium Eligible");
+				
+			}
+		}
+		
+		
+		else {
+			System.out.println("not eligible");
+		}
+	}
+
+}
