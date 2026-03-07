34f750a9450931476e0f638c88eb2af40321bf57
umairrrkhan <umair64066@gmail.com>
2026-03-07 13:34:46 +0530

    practicing first problem of loop is easy

---
+++ src/Practice/loop.java
@@ -0,0 +1,20 @@
+package Practice;
+
+public class loop {
+	
+	public static void main(String[] args	) {
+		
+		
+		int n = 10;
+		int sum = 0;
+		
+		for (int i = 1; i<=n; i++) {
+			sum+=i;
+			System.out.println(sum +"+" + i);
+		}
+		
+		
+		System.out.println(sum);
+	}
+
+}
