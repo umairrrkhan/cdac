f749b72d6d5c51ae9695de117363f25087f13995
umairrrkhan <umair64066@gmail.com>
2026-03-08 11:06:12 +0530

    pattern 5 inverted pyramid

--- src/practice/Pattern4.java
+++ src/practice/Pattern4.java
@@ -6,7 +6,7 @@ public class Pattern4 {
 	
 	public static void main(String[] args) {
 		for (int i = 1 ; i<=5 ; i++) {
-			for (int j = 1 ; j<=5-i ; j++) {
+			for (int j = 1 ; j<=5-i ; j++) { // this line is for the space same formula here for the both line okay .
 				System.out.print(" ");
 			}
 			for (int k = 1 ; k<=i; k++) {
---
+++ src/practice/Pattern5.java
@@ -0,0 +1,33 @@
+package practice;
+
+
+/*
+ *  1
+   222
+  33333
+ 4444444
+555555555
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+public class Pattern5 {
+	
+	 public static void main(String[] args) {
+		for (int i = 1 ; i<=5 ; i++) {
+			for (int j = 1 ; j<=5-i ; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1 ; k <=2*i-1 ; k++) {
+				System.out.print(i);
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
