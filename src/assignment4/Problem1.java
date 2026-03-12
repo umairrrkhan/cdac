e778bc8a62812e4764572746a984d07839ad0ad8
umairrrkhan <umair64066@gmail.com>
2026-03-12 17:15:50 +0530

    printing the string

---
+++ src/assignment4/Problem1.java
@@ -0,0 +1,19 @@
+package assignment4;
+
+import java.util.Scanner;
+
+public class Problem1 {
+	
+	public static void main(String[] args) {
+		Scanner io = new Scanner(System.in);
+		
+		System.out.print("enter a string : ");
+		
+		String str = io.nextLine();
+		
+		System.out.println(str);
+		
+		io.close();
+	}
+
+}
