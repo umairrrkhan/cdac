eb32f0f402f4090fd3020f275d13efb222665e4d
umairrrkhan <umair64066@gmail.com>
2026-03-18 12:22:42 +0530

    revision array

---
+++ src/datasa/Arraypart1.java
@@ -0,0 +1,30 @@
+package datasa;
+
+import java.util.Scanner;
+
+public class Arraypart1 {
+	
+	public static void main(String [] args) {
+	
+	   Scanner io = new Scanner(System.in);
+	   
+	   int a[] = new int[5]; // litterally forget the simple thing the syntax i will now focus on one language only .
+	   
+	   System.out.println(a.length);
+	   
+	   for (int i = 0 ; i< a.length ; i++) {
+		   a[i] = io.nextInt();
+		   
+		   System.out.println(a[i]);
+	   }
+	   
+	   int b [] = new int[4];
+	   
+	   b[0] = 1;
+	   b[1] = 2;
+	   
+	   System.out.println(b[0]);
+			   
+	}
+
+}
