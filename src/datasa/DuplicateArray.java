6558f7b342e935004822341a6f171309c201e2bb
umairrrkhan <umair64066@gmail.com>
2026-03-19 21:52:44 +0530

    will look in future im not able to understand too much noise in this fi

--- src/datasa/DuplicateArray.java
+++ src/datasa/DuplicateArray.java
@@ -11,9 +11,13 @@ public class DuplicateArray {
 		
 		Input(arr);
 		
-		dublicate(arr , gh);
-		print(gh);
-		print(arr);
+		
+		
+		int result = sortedArray(arr);
+		System.out.println("Unique count: " + result);
+		print(arr , result);  
+		
+		
 		
 	}
 	
@@ -44,18 +48,37 @@ public class DuplicateArray {
 		}
 	}
 	
-	public static void dublicate(int n [] , int g[]) {
-		for (int i = 0 ; i< n.length ; i++) {
-			for ( int j = i+1 ; j<n.length ; j++) {
-				if ( n[i] != n[j]) {
-				 g[i]= n[i];
-				 
-				}
+	
+	
+	public static int removedumplicate(int n[] , int g[] ) {
+		int i = 0;
+		
+		g[0] = n[0];
+		
+		for ( int j = 1 ; j< n.length ; j++) {
+			if ( n[j] != g[i]) {
+				i++;
+				g[i] = n[j];
+				
 			}
 		}
+		return i+ 1;
 	}
 	
-	public static void print(int g[]) {
+	public static int sortedArray(int n[]) {
+		int i = 0;
+		
+		for ( int j = 1 ; j< n.length ; j++) {
+			if (n[j] != n[i]) {
+				i++;
+				n[i] = n[j];
+			}
+		}
+		
+		return i + 1;
+	}
+	
+	public static void print(int g[] , int length) {
 		for (int i = 0 ; i< g.length ; i++) {
 			System.out.println(g[i]);
 		}
