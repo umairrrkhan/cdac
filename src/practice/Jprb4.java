62ae83992637d65618adf1646921b12d68b9067d
umairrrkhan <umair64066@gmail.com>
2026-03-30 22:02:07 +0530

    overiding and inheritence and super meathod

---
+++ src/practice/Jprb4.java
@@ -0,0 +1,63 @@
+package practice;
+
+
+class Animal{
+	String name ;
+	Animal(String name ){
+		this.name= name;
+		}
+	
+	void display() {
+		System.out.println(this.name);
+	}
+	
+	void sound() {
+		System.out.println("animal making sound");
+	}
+}
+
+class Dog extends Animal{
+	Dog(String name){
+		super(name);
+	}
+	
+	void sound() {
+		System.out.println("woof woof");
+	}
+	
+	void display() {
+		super.display();
+	}
+}
+
+class Cat extends Animal{
+	Cat(String name){
+		super(name);
+	}
+	
+	void sound() {
+		System.out.println("meow ");
+	}
+	
+	void display() {
+		super.display();
+	}
+}
+
+public class Jprb4 {
+	
+	public static void main(String[] args) {
+		
+		Animal a1 = new Animal("crow");
+		Animal a2 = new Dog("dog");
+		Cat a3 = new Cat("cat");
+		a1.display();
+		a1.sound();
+		a2.sound();
+		a2.display();
+		a3.sound();
+		a3.display();
+		
+	}
+
+}
