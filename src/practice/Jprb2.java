a9d69d83e6a3e655b92bba0e092ef5137c437984
umairrrkhan <umair64066@gmail.com>
2026-03-30 12:43:18 +0530

    overloading and overriding in same here

--- src/practice/Jprb2.java
+++ src/practice/Jprb2.java
@@ -14,6 +14,12 @@ class Calculator{
    }
 }
 
+class AdvanceCal extends Calculator{
+	static int add(int a , int b ) {
+		return a*b+ a*b;
+	}
+}
+
 public class Jprb2 {
 	
 	public static void main(String [] args) {
@@ -22,6 +28,8 @@ public class Jprb2 {
 		System.out.println(Calculator.add(1, 2, 4));
 		System.out.println(Calculator.add(12, 34));
 		
+		Calculator c1 = new AdvanceCal();
+		// System.out.println(c1.add(2,2)); to call this u have to remove the static from almost everymeathod 
 	}
 
 }
