cc97d71831e049a8d5f3f4ba6c8029b7ba01d7bf
umairrrkhan <umair64066@gmail.com>
2026-03-30 22:11:22 +0530

    Array + Loop = Polymorphism in action

--- src/practice/Jprb4.java
+++ src/practice/Jprb4.java
@@ -48,15 +48,27 @@ public class Jprb4 {
 	
 	public static void main(String[] args) {
 		
-		Animal a1 = new Animal("crow");
-		Animal a2 = new Dog("dog");
-		Cat a3 = new Cat("cat");
-		a1.display();
-		a1.sound();
-		a2.sound();
-		a2.display();
-		a3.sound();
-		a3.display();
+//		Animal a1 = new Animal("crow");
+//		Animal a2 = new Dog("dog");
+//		Cat a3 = new Cat("cat");
+//		a1.display();
+//		a1.sound();
+//		a2.sound();
+//		a2.display();
+//		a3.sound();
+//		a3.display();
+		
+		Animal [] a = new Animal[3];
+		a[0] = new Dog("dog");
+		a[1] = new Animal("animal");
+		a[2] = new Cat("cat");
+		
+		
+		for ( int i = 0 ; i < a.length ; i++) {
+			a[i].display();
+			a[i].sound();
+			
+		}
 		
 	}
 
