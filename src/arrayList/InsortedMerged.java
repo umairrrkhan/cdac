6fab24b10eb3c8333bac6d541d7b90192ad0edf3
umairrrkhan <umair64066@gmail.com>
2026-04-04 23:48:43 +0530

    in SOrted array

---
+++ src/arrayList/InsortedMerged.java
@@ -0,0 +1,17 @@
+package arrayList;
+
+public class InsortedMerged {
+	
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		int i = m -1 ;
+		int j = n -1 ;
+		int k = m+n -1 ;
+		
+		
+	}
+	
+	public static void main(String args[]) {
+		
+	}
+
+}
