74bc3eae95e6b3eda9fa7a0fbf6ee7a63706a2c4
umairrrkhan <umair64066@gmail.com>
2026-04-14 21:32:26 +0530

    linkedhashset remvoing duplicate unique+order

---
+++ src/arrayList/P14.java
@@ -0,0 +1,20 @@
+package arrayList;
+
+import java.util.*;
+
+import java.util.Arrays;
+
+public class P14 {
+	
+	
+	
+        public static void main(String args[]) {
+        	
+        	
+        	List<Integer> de = new ArrayList<>(new LinkedHashSet<>(List.of(1,2,3,4,5,6,7,8,6,4,3,34,4,4,4,4))); //arrya.aslist
+        	
+        	System.out.println(de);
+        	
+        	
+        }
+}
