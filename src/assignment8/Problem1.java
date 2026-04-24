7ca2d549ae39e979481c557cd6c58dff8fb42bd4
umairrrkhan <umair64066@gmail.com>
2026-04-24 23:24:54 +0530

    very basic

---
+++ src/assignment8/Problem1.java
@@ -0,0 +1,22 @@
+package assignment8;
+
+import java.util.*;
+
+public class Problem1 {
+	
+	static int result(int n , int m ) {
+		return n* m ;
+	}
+	
+	public static void main(String args[]) {
+		Scanner io = new Scanner(System.in);
+		System.out.println("skjdflaskfj");
+		
+		int d = io.nextInt();
+		System.out.println("ksjflsjfkl");
+		int s = io.nextInt();
+		
+		System.out.println(result(d,s));
+	}
+
+}
