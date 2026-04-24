92000828fa178dd3f96167644af7d065736e10a1
umairrrkhan <umair64066@gmail.com>
2026-04-24 23:43:36 +0530

    assignment problem4

---
+++ src/assignment8/Problem4.java
@@ -0,0 +1,16 @@
+package assignment8;
+
+public class Problem4 {
+
+	static void result() {
+		for(int i = 1 ; i<=100 ; i++) {
+			if(i%5==0) {
+
+				System.out.println(i);
+			}
+		}
+	}
+	public static  void main(String args[]){
+		result();
+	}
+}
