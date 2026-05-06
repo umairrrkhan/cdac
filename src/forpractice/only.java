574587ebfc315e81fbeb0be8453d3d353a0c26f5
umairrrkhan <umair64066@gmail.com>
2026-05-06 23:43:15 +0530

    exam doubt fixing

---
+++ src/forpractice/only.java
@@ -0,0 +1,12 @@
+package forpractice;
+
+public class only {
+	
+	public static void main(String args[]) {
+		String s = "umair";
+		
+		String b = s.concat("crazy");
+		System.out.println(s);
+	}
+
+}
---
+++ src/practice/Rishabhpractice.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Rishabhpractice {
+	static int x = 10;
+    static {
+        x += 5;
+    }
+	public static void main(String args[]) {
+		System.out.println(10+20 + "5" +5+5);
+		System.out.println(10+30+"cdac"+10+30);
+		System.out.println(x);
+	}
+
+}
