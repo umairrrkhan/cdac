201641e262770c9afe8500bacb1c63403c68da3e
umairrrkhan <umair64066@gmail.com>
2026-05-26 23:32:49 +0530

    prefix

--- src/revision/Prefix.java
+++ src/revision/Prefix.java
@@ -1,5 +1,28 @@
 package revision;
 
 public class Prefix {
+	
+	public static int[] sum(int arr[]) {
+		int[] prefix = new int [arr.length];
+		
+		prefix[0] = arr[0];
+		
+		for(int i = 1 ; i<arr.length ; i++) {
+			prefix[i] = prefix[i-1] + arr[i];
+		}
+		
+		return prefix;
+	}
+	
+	public static int rangeSum(int prefix[] , int l , int r) {
+		if(l == 0 ) return prefix[r];
+		
+		return prefix[r] - prefix[l-1];
+		
+	}
+	
+	public static void main(String args[]) {
+		
+	}
 
 }
