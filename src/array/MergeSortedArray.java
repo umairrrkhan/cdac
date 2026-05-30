60b90a9a3b3b6aa32876ec5868487bc9f5aae3bd
umairrrkhan <umair64066@gmail.com>
2026-05-30 21:39:23 +0530

    merge sorted array blindly did this

--- src/array/MergeSortedArray.java
+++ src/array/MergeSortedArray.java
@@ -37,6 +37,41 @@ public class MergeSortedArray {
 	}
 	
 	
+	
+	public static int[] practice(int a1[] , int a2[]) {
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		
+		int result[] = new int [a1.length+a2.length];
+		
+		while(i<a1.length&& j<a2.length ) {
+			if(a1[i]<=a2[j]) {
+				result[k] = a1[i];
+				i++;
+			}else {
+				result[k] = a2[j];
+				j++;
+			}
+			
+			k++;
+		}
+		
+		while(i<a1.length) {
+			result[k] = a1[i];
+			i++;
+			k++;
+		}
+		
+		while(j<a2.length) {
+			result[k] = a2[j];
+			k++;
+			j++;
+		}
+		
+		return result;
+	}
+	
 	public static void main(String args[]) {
 		int num1[] = {1,3,4};
 		int num2[] = {7,3,9};
