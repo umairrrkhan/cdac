9aab43332c94267a31ed4657214fa902ac82bec7
umairrrkhan <umair64066@gmail.com>
2026-05-30 22:41:04 +0530

    majority element i did one more time

--- src/array/MajorityElement.java
+++ src/array/MajorityElement.java
@@ -20,6 +20,23 @@ public class MajorityElement {
 		return candidate ;
 	}
 	
+	public static int practice(int arr[]) {
+		int candidate = arr[0];
+		int count = 1;
+		for(int i = 1 ; i <arr.length ; i++) {
+			if(count == 0) {
+				candidate = arr[i];
+				count = 1;
+			}else if (arr[i]== candidate ) {
+				count++;
+			}else {
+				count--;
+			}
+		}
+		
+		return count;
+	}
+	
 	public static void main(String args[]) {
 		int  m[] = {2, 2, 1, 1, 1, 2, 1};
 		
