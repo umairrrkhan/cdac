9f347f97f2ce713e29ab554db8f83cbe49ef0359
umairrrkhan <umair64066@gmail.com>
2026-05-31 22:35:15 +0530

    reverse linked list

--- src/linkedlist/AtomicStructure.java
+++ src/linkedlist/AtomicStructure.java
@@ -45,5 +45,19 @@ class Node{
 		return newnode;
 	}
 	
+	Node reverse(Node head) {
+		Node prev = null;
+		Node curr = head;
+		Node next = null;
+		
+		while(curr!= null) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		return prev;
+	}
+	
 	
 }
