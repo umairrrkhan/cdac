23c7dd663da891f70302903403708ea29b6ab003
umairrrkhan <umair64066@gmail.com>
2026-05-31 22:43:03 +0530

    this one is the has cycle problem int he linked list

--- src/linkedlist/AtomicStructure.java
+++ src/linkedlist/AtomicStructure.java
@@ -70,5 +70,20 @@ class Node{
 		return slow;
 	}
 	
+	public static boolean hasFloydCycle(Node head) {
+		Node slow = head ;
+		Node fast = head;
+		
+		while(fast!=null&& fast.next!=null) {
+			slow = slow.next ;
+			fast = fast.next.next;
+			if(slow == fast) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
 	
 }
