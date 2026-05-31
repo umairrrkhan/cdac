e5878c0e092b806bd44206703480a5b94ca0f46e
umairrrkhan <umair64066@gmail.com>
2026-05-31 22:36:53 +0530

    findmiddle one in the linked list

--- src/linkedlist/AtomicStructure.java
+++ src/linkedlist/AtomicStructure.java
@@ -59,5 +59,16 @@ class Node{
 		return prev;
 	}
 	
+	Node findmiddle(Node head) {
+		Node slow  = head;
+		Node fast = head ;
+		
+		while(fast!=null && fast.next!=null) {
+			slow = slow.next ;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+	
 	
 }
