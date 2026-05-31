2a01f5df57c3d68fbd2b221ca616d4d89c3bc465
umairrrkhan <umair64066@gmail.com>
2026-05-31 21:47:15 +0530

    added the printing method

--- src/linkedlist/AtomicStructure.java
+++ src/linkedlist/AtomicStructure.java
@@ -2,6 +2,20 @@ package linkedlist;
 
 public class AtomicStructure {
 
+	public static void main(String args[]) {
+		Node head = new Node(10);
+		Node second = new Node(20);
+		Node third = new Node(30);
+		
+		head.next = second;
+		second.next = third;
+		
+		System.out.println("My Linked List:");
+		
+		Node.traverse(head);
+		
+		System.out.println("end here ");
+	}
 	
 }
 
@@ -14,4 +28,16 @@ class Node{
 		this.next = null;
 		
 	}
+	
+	public static void traverse(Node head) {
+		Node current = head;
+		while(current!=null) {
+			System.out.println(current.val);
+			current = current.next;
+		}
+		
+		System.out.println("null");
+	}
+	
+	
 }
