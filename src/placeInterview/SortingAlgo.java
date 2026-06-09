6246f23b9952f6ff4dbb062c0e31533ca679c717
umairrrkhan <umair64066@gmail.com>
2026-06-09 22:50:54 +0530

    bubble sort

---
+++ src/placeInterview/SortingAlgo.java
@@ -0,0 +1,23 @@
+package placeInterview;
+
+public class SortingAlgo {
+	
+	void bubblesort(int arr[]) {
+		int n = arr.length;
+		
+		for(int i = 0 ; i<n-1-i ; i++) {
+			boolean swapped = false;
+			for(int  j = 0 ; j<n-1; j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1]= temp ;
+					
+					swapped = true;
+				}
+			}
+			
+			if(!swapped) break;
+		}
+	}
+}
