a99c5a025d2be3f3ab8a9fa7f6cbb7cf4bc530f6
umairrrkhan <umair64066@gmail.com>
2026-06-09 22:56:57 +0530

    selection sort is kinda easy too

--- src/placeInterview/SortingAlgo.java
+++ src/placeInterview/SortingAlgo.java
@@ -20,4 +20,22 @@ public class SortingAlgo {
 			if(!swapped) break;
 		}
 	}
+	
+	void selectionsort(int arr[] ) {
+		int n = arr.length ;
+		
+		for (int i = 0 ; i<n-1; i++) {
+			int minimum = i;
+			
+			for(int j = i+1; j<n; j++) {
+				if(arr[j]<arr[minimum]) {
+					minimum = j;
+				}
+			}
+			
+			int temp = arr[i];
+			arr[i] = arr[minimum];
+			arr[minimum] = temp;
+		}
+	}
 }
