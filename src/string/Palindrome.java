c87ba2b2b8a8124bf0b6cd42864438200290fcab
umairrrkhan <umair64066@gmail.com>
2026-06-11 23:59:29 +0530

    new quesrtion

--- src/string/Palindrome.java
+++ src/string/Palindrome.java
@@ -4,6 +4,21 @@ package string;
 
 public class Palindrome {
 	
+	public boolean isPalindrome(String s) {
+		int left = 0 ; 
+		int right = s.length()-1;
+		
+		while(left < right) {
+			if(s.charAt(left)!=s.charAt(right)) {
+				return false;
+			}
+			left++;
+			right--;
+		}
+		
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		
 		String str = "racecayr";
