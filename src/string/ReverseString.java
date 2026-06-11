938d89e538c7ce5bcd108193bca62e8f12458afc
umairrrkhan <umair64066@gmail.com>
2026-06-11 23:51:31 +0530

    reverse String first problem easy level

---
+++ src/string/ReverseString.java
@@ -0,0 +1,20 @@
+package string;
+
+public class ReverseString {
+	
+	public static String reverseString(String s) {
+		char [] c = s.toCharArray();
+		int left = 0 , right = c.length-1;
+		
+		while(left < right) {
+			char temp = c[left];
+			c[left] = c[right];
+			c[right] = temp;
+			left++;
+			right--;
+		}
+		
+		return new String(s);
+	}
+
+}
