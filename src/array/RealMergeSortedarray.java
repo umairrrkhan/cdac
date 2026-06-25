a6e9cd651acb6d58ab807361ddb91c34dfa00fd6
umairrrkhan <umair64066@gmail.com>
2026-06-25 10:06:43 +0530

    this is the real merge sorted array problem

---
+++ src/array/RealMergeSortedarray.java
@@ -0,0 +1,29 @@
+package array;
+
+public class RealMergeSortedarray {
+	
+	public void merge(int num1[] , int m , int num2 [] , int n ) {
+		int i = m-1;
+		int j = n-1;
+		int k = m+n -1 ;
+		
+		while (i >= 0 && j >= 0) {
+			if(num1[i]>= num2[j]) {
+				num1[k] = num1[i];
+				i--;
+			}else {
+				num1[k] = num2[j];
+				j--;
+			}
+			
+			k--;
+		}
+		
+		while(j>= 0) {
+			num1[k]= num2[j];
+			j--;
+			k--;
+		}
+	
+	}
+}
