24a9d8ab368588db05a7afc5d07b06e69b39fda3
umairrrkhan <umair64066@gmail.com>
2026-06-26 23:56:38 +0530

    here find the index which is equal to both side

---
+++ src/array/EquilibriumIndexArray.java
@@ -0,0 +1,28 @@
+package array;
+
+public class EquilibriumIndexArray {
+	
+	public static int result(int [] arr) {
+		int totalsum = 0;
+		
+		for(int i : arr) {
+			totalsum+=i;
+		}
+		
+		int leftsum = 0;
+		
+		for(int i = 0; i < arr.length ; i++) {
+			int rightsum = totalsum - leftsum - arr[i];
+			
+			if(leftsum== rightsum) {
+				return i ;
+			}
+			
+			leftsum+=arr[i];
+		}
+		
+		
+		return -1;
+	}
+
+}
