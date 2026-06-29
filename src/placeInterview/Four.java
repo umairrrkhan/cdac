779091fe815a76c253b8164ffec87f7c017fcc18
umairrrkhan <umair64066@gmail.com>
2026-06-29 22:13:16 +0530

    binary search

---
+++ src/placeInterview/Four.java
@@ -0,0 +1,22 @@
+package placeInterview;
+
+public class Four {
+	public static boolean result(int arr[] , int target) {
+		int left = 0;
+		int right = arr.length -1;
+		
+		while(left <= right) {
+			int mid = left + (right-left)/2;
+			if(arr[mid] == target) {
+				return true ; // arr[mid]
+			}else if(arr[mid]< target) {
+				left = mid+1;
+			}else {
+				right = mid -1;
+			}
+		}
+		
+		return false;
+	}
+
+}
