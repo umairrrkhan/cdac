2f21b4ee0f377cf1208bfb9b370a8244a3680da8
umairrrkhan <umair64066@gmail.com>
2026-06-30 23:59:04 +0530

    a2b3c4

---
+++ src/string/CompressString.java
@@ -0,0 +1,25 @@
+package string;
+
+public class CompressString {
+	
+	public static String compress(String s ) {
+		StringBuilder result = new StringBuilder();
+		
+		int i = 0;
+		while(i< s.length()) {
+			char current = s.charAt(i);
+			int count = 0;
+			
+			while(i<s.length() && s.charAt(i) == current) {
+				count ++;
+				i++;
+			}
+			
+			result.append(current);
+			if(count > 1) result.append(count);
+		}
+		
+		return result.toString();
+	}
+
+}
