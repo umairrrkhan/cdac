586172cdafb4dddb49b4d402c458df9c8c61669c
umairrrkhan <umair64066@gmail.com>
2026-06-30 22:27:15 +0530

    String first non repeating chrachter

---
+++ src/string/FirstNonRepeatingCharachter.java
@@ -0,0 +1,22 @@
+package string;
+
+public class FirstNonRepeatingCharachter {
+	
+	public static int firstUniq(String s) {
+		int freq[] = new int[26];
+		
+		for(char c : s.toCharArray()) {
+			freq[c-'a']++;
+		}
+		
+		for(int i = 0 ; i< s.length() ; i++) {
+			char c = s.charAt(i);
+			if(freq[c-'a']==1) {
+				return i ;
+			}
+		}
+		
+		return -1;
+	}
+
+}
