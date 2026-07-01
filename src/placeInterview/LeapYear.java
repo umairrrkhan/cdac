874b95e131c4591895db8e201c4d8ba4791eb9b5
umairrrkhan <umair64066@gmail.com>
2026-07-02 00:45:19 +0530

    leap year problem here

---
+++ src/placeInterview/LeapYear.java
@@ -0,0 +1,13 @@
+package placeInterview;
+
+public class LeapYear {
+	
+	public static boolean result(int year) {
+		if(year % 400 == 0 ) return true;
+		if(year % 100 == 0 ) return false;
+		
+		return year % 4 == 0 ;
+		
+	}
+
+}
