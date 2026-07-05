cfa2143e5b3a44de36f7a74ff7f2529a610f4b62
umairrrkhan <umair64066@gmail.com>
2026-07-06 01:16:40 +0530

    interview version for the count the word

--- src/string/CountWord.java
+++ src/string/CountWord.java
@@ -20,5 +20,26 @@ public class CountWord {
 		
 		return count;
 	}
+	
+	
+	public int Interview(String s ) {
+		if(s == null || s.trim().isEmpty()) {
+			return 0 ;
+		}
+		
+		int count = 0 ;
+		boolean inWord = false;
+		
+		for(char c : s.toCharArray()) {
+			if(!Character.isWhitespace(c) && !inWord) {
+				count ++;
+				inWord  = true;
+			} else if(Character.isWhitespace(c)) {
+				inWord = false;
+			}
+		}
+		
+		return count;
+	}
 
 }
