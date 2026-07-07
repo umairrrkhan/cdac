d52cf3c650226a0bb6ab5ca96184e9d59c4b7fc4
umairrrkhan <umair64066@gmail.com>
2026-07-07 11:24:57 +0530

    interview safe version for the unique char

--- src/string/FirstNonRepeatingCharachter.java
+++ src/string/FirstNonRepeatingCharachter.java
@@ -1,5 +1,7 @@
 package string;
 
+
+import java.util.*;
 public class FirstNonRepeatingCharachter {
 	
 	public static int firstUniq(String s) {
@@ -12,6 +14,22 @@ public class FirstNonRepeatingCharachter {
 		for(int i = 0 ; i< s.length() ; i++) {
 			char c = s.charAt(i);
 			if(freq[c-'a']==1) {
+				return i ;
+			}
+		}
+		
+		return -1;
+	}
+	
+	public static int firstunique(String s) {
+		Map<Character, Integer> freq = new HashMap<>();
+		
+		for(char c : s.toCharArray()) {
+			freq.put(c , freq.getOrDefault(c, 0)+1);
+		}
+		
+		for(int i = 0 ; i < s.length() ; i++) {
+			if(freq.get(s.charAt(i))==1) {
 				return i ;
 			}
 		}
