5c466a84e47038ccc1aab1787b196f7b956e3fdd
umairrrkhan <umair64066@gmail.com>
2026-07-19 23:49:55 +0530

    here si the code

--- src/mySql/HighestOrderPerCustomer.java
+++ src/mySql/HighestOrderPerCustomer.java
@@ -1,5 +1,21 @@
 package mySql;
 
 public class HighestOrderPerCustomer {
+	
+	/*
+	 * select order_id , customer_id , max(amount)
+	 * from orders
+	 * group by customer_id
+	 * 
+	 *  Map<Integer , Order > result = orders.stream()
+	 *  .collect(Collectors.groupingBy(
+	 *  Order:: getCustomerId, Collecgtors.collectingAndThen(
+	 *  Collectors.maxBy(Comparator.comparingInt(Order::getAmount)),
+	 *  Optional::get)));
+	 *  
+	 *  
+	 *  result.forEach((k, v) -> sysout(k , v);
+	 * 
+	 */
 
 }
