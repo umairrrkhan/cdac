303e175d8a53d0e88f7079b58e0c038442b28695
umairrrkhan <umair64066@gmail.com>
2026-07-23 12:15:35 +0530

    fibonacci in recursive and sum to n in recursive

--- src/placeInterview/Fibonacci.java
+++ src/placeInterview/Fibonacci.java
@@ -15,5 +15,26 @@ public class Fibonacci {
 			b = next;
 		}
 	}
+	
+	
+	public static int fibonacci(int n) {
+		if(n <= 1) {
+			return n ;
+		}
+		
+		return fibonacci(n-1) + fibonacci(n-2);
+	}
+	
+	public static int sumToN(int n ) {
+		if(n== 0 ) {
+			return 0;
+		}
+		
+		return n + sumToN(n-1);
+	}
+	
+	public static void main(String args[]) {
+		
+	}
 
 }
