93a4c4d45ac2fc3d4bb178bfbf8f14f3e2196f9b
Umair khan <89532763+umairrrkhan@users.noreply.github.com>
2026-07-25 22:18:04 +0530

    fix parameter order in merge method

--- src/array/MergeInPlace.java
+++ src/array/MergeInPlace.java
@@ -1,10 +1,14 @@
 package array;
 
+
+//  Merge Sorted Array (LeetCode 88)
+
+
 public class MergeInPlace {
 	
-	public static void merge(int nums1[] , int n , int nums2[] , int m) {
+	public static void merge(int nums1[] , int m , int nums2[] , int n) {
 		int i =  m-1;
-		int j = n=1;
+		int j = n-1;
 		int k = m+n-1;
 		
 		while (i >= 0 && j >= 0) {
