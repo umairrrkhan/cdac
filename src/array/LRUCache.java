babd63774b220cb6c5fac96c6cd3af0b85510ac5
umairrrkhan <umair64066@gmail.com>
2026-07-25 23:30:25 +0530

    lru cache for me its important

---
+++ src/array/LRUCache.java
@@ -0,0 +1,32 @@
+package array;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache {
+	
+	private final int capacity;
+	private final LinkedHashMap<Integer , Integer> map;
+	
+	public LRUCache(int capacity) {
+		this.capacity= capacity;
+		
+		this.map = new LinkedHashMap<Integer , Integer> (capacity , 0.75f , true){
+			
+			@Override
+			protected boolean removeEldestEntry(Map.Entry<Integer , Integer> eldest) {
+				return size() > capacity;
+			}
+		};
+		
+	}
+	
+	public int get(int key) {
+		return map.getOrDefault(key, -1);
+	}
+	
+	public void put(int key, int value) {
+        map.put(key, value);
+    }
+
+}
