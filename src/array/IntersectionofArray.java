6fb245ae4391c4aac03d130746af5ef689d5e79a
umairrrkhan <umair64066@gmail.com>
2026-07-26 02:05:03 +0530

    new internview version

--- src/array/IntersectionofArray.java
+++ src/array/IntersectionofArray.java
@@ -2,6 +2,7 @@ package array;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class IntersectionofArray { //sorted
 	
@@ -27,5 +28,31 @@ public class IntersectionofArray { //sorted
 		
 	}
 	
+	// this is the interview version 
 	
+	public static int[] intersection (int [] nums1 , int [] nums2) {
+		Set<Integer> set = new HashSet<>();
+		
+		for(int num : nums2) {
+			set.add(num);
+		}
+		
+		
+		Set<Integer> resultSet = new Hashset<>();
+		
+		for(int num : nums1) {
+			if(set.contains(num)) {
+				resultSet.add(num);
+			}
+		}
+		
+		int [] result = new int [ resultSet.size()];
+		
+		int i = 0 ;
+		for (int num : resultSet) {
+			result[i++] = num;
+		}
+		
+		return result;
+	}
 }
