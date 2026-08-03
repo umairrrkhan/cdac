967114dcf9c6a3894813ce8912480f1b59d5ada0
umairrrkhan <umair64066@gmail.com>
2026-08-03 14:43:35 +0530

    unique path + dp + medium

--- src/dp/UniquePath.java
+++ src/dp/UniquePath.java
@@ -1,5 +1,25 @@
 package dp;
 
 public class UniquePath {
+	
+	
+public int uniquePaths(int m, int n) {
+	
+	int dp[] = new int[n];
+	
+	for(int column = 0 ; column < n ; column++ ) {
+		dp[column] = 1;
+	}
+	
+	
+	for(int row = 1 ; row< m ; row++) {
+		for(int column = 1 ; column < n ; column++) {
+			dp[column] = dp[column] + dp[column-1];
+		}
+	}
+	
+	return dp[n-1];
+        
+    }
 
 }
