f170c9b330dd913873d54519aa8a50dcabdc58b9
umairrrkhan <umair64066@gmail.com>
2026-08-06 16:58:51 +0530

    climbing stairs dp version

--- src/dp/ClimbingStairs.java
+++ src/dp/ClimbingStairs.java
@@ -17,5 +17,23 @@ public class ClimbingStairs {
 		
 		return onestepbefore;
 	}
-
+	
+	
+	
+	public static int dpv(int n) {
+		if(n <= 2){ return n ;}
+		
+		int dp[] = new int[n+1];
+		dp[1] = 1;
+		dp[2] = 2;
+		
+		for(int i= 3 ; i<=n ; i++) {
+			dp[i]= dp[i-1] + dp[i-2];
+		}
+		
+		return dp[n];
+			
+		}
+		
+	
 }
