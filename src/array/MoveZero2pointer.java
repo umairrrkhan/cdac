6159c4ce4aa034e4a74c517eb55e980d26a35fdc
Umair khan <89532763+umairrrkhan@users.noreply.github.com>
2026-09-22 19:27:36 +0530

    Add moveZeroes method to rearrange array elements
    Implemented moveZeroes method to shift non-zero elements to the front and fill the rest with zeros.

--- src/array/MoveZero2pointer.java
+++ src/array/MoveZero2pointer.java
@@ -15,6 +15,20 @@ public class MoveZero2pointer {
 			}
 		}
 	}
+
+	public static void moveZeroes(int[] arr) {
+    int index = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] != 0) {
+            arr[index++] = arr[i];
+        }
+    }
+
+    while (index < arr.length) {
+        arr[index++] = 0;
+    }
+}
 	
 	public static void main(String args[]) {
 		
